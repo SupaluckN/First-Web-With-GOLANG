7bd039fa4795869eb383329565c41a72797ceb57
Supaluck Singjan <supalucksingjan@gmail.com>
2020-07-12 18:29:20 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,10 +4,7 @@ package main
 
 import "net/http"
 
-/* Show Welcome Message on WebPage.
-   -- handlerIndex --
-*/
-
+//handlerIndex 
 func handlerIndex(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Welcome to This Site.</h1>"))
 	w.Write([]byte("<p>This website created by GOLANG</p>"))
